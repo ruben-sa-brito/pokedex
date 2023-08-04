1c3751d2b4e0323f51e46bc4958ba529fb117d8f
Timewound <rubenjms1@gmail.com>
2023-08-04 08:37:12 -0300

    incluindo botao search, e adicionando funcionalidade a todos os botões

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
-
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
+const btnSearch= document.querySelector('.btn-search');
+const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
+var idPokemon = 0;
 
 
 const fetchPokemon = async (pokemon) => {
@@ -22,6 +25,8 @@ const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    idPokemon = data.id;
+    console.log(idPokemon)
     
 
 }
@@ -32,4 +37,27 @@ form.addEventListener('submit', (event)=>{
     renderPokemon(input.value);
     input.value = '';
     
+})
+
+btnSearch.addEventListener('click', (event)=>{
+    event.preventDefault();
+    
+    if (input.value.length > 0) { 
+        renderPokemon(input.value);
+        input.value = '';
+    }
+    
+})
+
+btnNext.addEventListener('click',()=>{
+    console.log(idPokemon + 1)
+    renderPokemon(idPokemon + 1)
+   
+})
+
+btnPrev.addEventListener('click',()=>{
+    if (idPokemon > 1){
+        renderPokemon(idPokemon - 1)
+    }
+   
 })
